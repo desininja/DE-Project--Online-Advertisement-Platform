c2d58685ea1dacc329bde2f80be1ab1ca7c746e3
Himanshu Bhatt <66287451+desininja@users.noreply.github.com>
2025-09-08 01:16:41 +0530

    Completed the project with SQL queries as well.

--- Main Scripts/feedbackWriter.py
+++ Main Scripts/feedbackWriter.py
@@ -8,8 +8,9 @@ load_dotenv()
 
 if __name__ == "__main__":
 
-    KAFKA_PACKAGE = "org.apache.spark:spark-sql-kafka-0-10_2.12:4.0.1"
-
+    # The package needs to be specified either here in the config or with --packages
+    # Let's add it here to make the script self-contained.
+    # Make sure the version matches your PySpark installation.
 
     spark = SparkSession \
         .builder \
@@ -26,7 +27,8 @@ if __name__ == "__main__":
         .format("kafka") \
         .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVER) \
         .option("subscribe", KAFKA_TOPIC_NAME) \
-        .option("startingOffsets", "latest") \
+        .option("startingOffsets", "earliest") \
+        .option("failOnDataLoss", "false") \
         .load()
 
     json_schema = StructType([
@@ -54,14 +56,25 @@ if __name__ == "__main__":
         .select(from_json(col("json_string"),json_schema).alias("data"))\
         .select("data.*")
 
-    output_path = "/Users/himanshu/github/DE-Project--Online-Advertisement-Platform/logs_feedback_handler/output"
-    checkpoint_path ="/Users/himanshu/github/DE-Project--Online-Advertisement-Platform/checkpointLocation"
-    query = desearialised_df.writeStream \
+    # --- Start Query 1: Writing to JSON files ---
+    output_path = os.getenv("FILE_OUTPUT_PATH")
+    checkpoint_path = os.getenv("CHECKPOINT_PATH")
+    
+    file_sink_query = desearialised_df.writeStream \
         .format("json") \
         .option("path", output_path) \
-        .option("checkpointLocation",checkpoint_path ) \
+        .option("checkpointLocation", checkpoint_path) \
         .trigger(processingTime="5 seconds") \
-        .start() \
-        .awaitTermination()
+        .start()
+
+    # --- Start Query 2: Writing to the console ---
+    console_sink_query = desearialised_df.writeStream \
+        .format("console") \
+        .outputMode("append") \
+        .trigger(processingTime="5 seconds") \
+        .start()
+
+    # --- Wait for any of the streams to terminate ---
+    spark.streams.awaitAnyTermination()
 
     spark.stop()
